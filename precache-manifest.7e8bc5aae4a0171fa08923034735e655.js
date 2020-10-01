27e3ac41edfac9def4c4ab7440a1ab8e013c7369
Esteban Benjumea <benjumeac12@gmail.com>
2020-10-01 11:47:59 -0500

    Updates

---
+++ precache-manifest.7e8bc5aae4a0171fa08923034735e655.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8fdf0b1602f1eb17805b84ca90878c39",
+    "url": "/prueba-react/index.html"
+  },
+  {
+    "revision": "44907057ad6229258e53",
+    "url": "/prueba-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "386e6d9fa318e5af4ef2",
+    "url": "/prueba-react/static/js/2.d815da71.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prueba-react/static/js/2.d815da71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44907057ad6229258e53",
+    "url": "/prueba-react/static/js/main.21f9fbe6.chunk.js"
+  },
+  {
+    "revision": "2b63a047c917e9d952a0",
+    "url": "/prueba-react/static/js/runtime-main.578b8869.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/prueba-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
